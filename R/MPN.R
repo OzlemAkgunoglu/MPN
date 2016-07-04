b2ab86c33479955b0022ed465b6d17d2bacfa90b
Etienne Low-Decarie <etienne.low-decarie@mail.mcgill.ca>
2016-07-04 17:38:17 +0100

    First commit

---
+++ R/MPN.R
@@ -0,0 +1,32 @@
+#' Most Probable Number (MPN) calculator
+#'
+#' @param g the number of positive (or growth) tubes in the jth dilution
+#' @param m the amount of the original sample put in the tubes at each dilution.
+#' @param m the number of tubes at each dilution
+#' @return The most probable  \code{x} and \code{y}.
+#' @examples
+#' MPN(g=c(5,3,0),m=c(0.1,0.01,0.001),t=10)
+
+MPN <- function(g,m,t) {
+  min_fun <- function(lambda){
+    abs(sum(g*m/(1-exp(-lambda*m)))-sum(t*m))}
+  solution <- optimize(f=min_fun,
+        lower=0,
+        upper=100) #setting this too high can lead it to stick to these high values
+  return(solution$minimum)
+}
+
+
+# MPN <- function(g,m,t) {
+#   min_fun <- function(lambda){
+#     abs(sum(g*m/(1-exp(-lambda*m)))-sum(t*m))}
+#   solution <- optim(fn=min_fun,
+#       par=c(lambda=1),
+#       method="Brent",
+#       lower=0,
+#       upper=100)
+# return(solution)
+# }
+
+
+
