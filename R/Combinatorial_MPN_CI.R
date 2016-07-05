47579de6fc70f0a0ff35df4dd56588d87c8a95cf
Etienne Low-Decarie <etienne.low-decarie@mail.mcgill.ca>
2016-07-05 11:17:43 +0100

    added upper bound based on Blodgett (2005b)

---
+++ R/Combinatorial_MPN_CI.R
@@ -0,0 +1,1 @@
+#Man, J.C., 1983. MPN tables, corrected. Eur. J. Appl. Microbiol. Biotechnol. 17, 301–305. doi:10.1007/BF00508025
--- R/MPN.R
+++ R/MPN.R
@@ -8,14 +8,17 @@
 #' MPN(g=c(5,3,0),m=c(0.1,0.01,0.001),t=10)
 
 MPN <- function(g,m,t) {
+  upper <- sum(g)/sum((t-g)*m)
   min_fun <- function(lambda){
     abs(sum(g*m/(1-exp(-lambda*m)))-sum(t*m))}
   solution <- optimize(f=min_fun,
         lower=0,
-        upper=100) #setting this too high can lead it to stick to these high values
+        upper=upper) #setting this too high can lead it to stick to these high values
   return(solution$minimum)
 }
 
+
+#Add upper and lower bound calculations
 
 # MPN <- function(g,m,t) {
 #   min_fun <- function(lambda){
